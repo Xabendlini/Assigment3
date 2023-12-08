7b54457235769ba40c61ae753681d40cfdd9da3c
Xabendlini <151826333+Xabendlini@users.noreply.github.com>
2023-12-08 13:12:24 +0200

    first commit

---
+++ Assigment3/Alcohol.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment3
+{
+    internal class AlcoholOfChoice
+    {
+        private string AlcoholType;
+        private int NumberOfDrinks;
+        private bool IceOrNoIce;
+
+        public AlcoholOfChoice(string alcoholType, int numberOfDrinks, bool iceOrNoIce)
+        {
+            AlcoholType = alcoholType;
+            NumberOfDrinks = numberOfDrinks;
+            IceOrNoIce = iceOrNoIce;
+        }
+
+        public string GetAlcoholChoice()
+        {
+            return AlcoholType;
+        }
+        public int GetNumberOfDrinks()
+        { 
+            return NumberOfDrinks; 
+        }
+        public bool GetIceOrNoIce()
+        { 
+            return IceOrNoIce;
+        }
+
+    }
+}
---
+++ Assigment3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assigment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlcoholOfChoice Milani = new AlcoholOfChoice("Henessy", 6, true);
+            Console.WriteLine("Milani" + " " + Milani.GetAlcoholChoice());
+
+
+            AlcoholOfChoice Gaba = new AlcoholOfChoice("Heineken", 12, false);
+            Console.WriteLine("Gaba"+ " " + Gaba.GetAlcoholChoice());
+
+
+            AlcoholOfChoice Olwethu = new AlcoholOfChoice("Breezer", 6, false);
+            Console.WriteLine("Olwethu" + " " + Olwethu.GetAlcoholChoice());
+
+            Console.WriteLine("GLASSES" + " " + Milani.GetNumberOfDrinks());
+            Console.WriteLine("GLASSES" + " " + Gaba.GetNumberOfDrinks());
+            Console.WriteLine("GLASSES" + " " + Olwethu.GetNumberOfDrinks());   
+
+            Console.WriteLine(" DOES IT HAVE ICE? " + " " + Milani.GetIceOrNoIce());
+            Console.WriteLine(" DOES IT HAVE ICE? " + " " + Gaba.GetIceOrNoIce());
+            Console.WriteLine(" DOES IT HAVE ICE? " + " " + Olwethu.GetIceOrNoIce());
+        }
+    }
+}
